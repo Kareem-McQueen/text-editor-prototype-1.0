e7c0ba2f9bcdeb64f38f4450084044d291b3bcf5
kareem-mcqueen <hello@kareemmcqueen.com>
2022-07-19 11:24:12 -0400

    fist commit

---
+++ app.js
@@ -0,0 +1,37 @@
+function bold() {
+    document.getElementById('textarea').style.fontWeight = "bold";
+}
+
+function italic() {
+    document.getElementById('textarea').style.fontStyle = "italic";
+}
+
+function la() {
+    document.getElementById('textarea').style.textAlign = "left";
+}
+
+function ca() {
+    document.getElementById('textarea').style.textAlign = "center";
+}
+
+function ra() {
+    document.getElementById('textarea').style.textAlign = "right";
+}
+
+function uppercase() {
+    document.getElementById('textarea').style.textTransform = "uppercase";
+}
+
+function lowercase() {
+    document.getElementById('textarea').style.textTransform = "lowercase";
+}
+
+function capitalize() {
+    document.getElementById('textarea').style.textTransform = "capitalize";
+}
+
+function cleartext() {
+    document.getElementById('textarea').value = "";
+}
+
+
